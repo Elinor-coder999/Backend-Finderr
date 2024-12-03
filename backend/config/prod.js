65f10c329af78f5d5cfc3db08733db76c2cc1e5e
Elinor <israeli.elinor@gmail.com>
2024-12-03 22:25:12 +0200

    Fixing db name

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -2,6 +2,6 @@
 
 module.exports =  {
   dbURL:'mongodb+srv://israelielinor:elinor123@cluster0.mk1t2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'  ,
-  dbName: 'gig_db'
+  dbName: 'elinor'
 }
 
